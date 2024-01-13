5a3d28c85af65db4fc8d68a0bed97b3ffcd00646
Twin J <junohanyere@gmail.com>
2024-01-13 16:31:29 +0100

    fixed pycodestyle errors

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+"""starts a flask web app
+"""
 
 from models import storage
 from models.state import State
@@ -9,6 +10,8 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
+
+
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -16,13 +19,17 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+    """removes the current SQLAlchemy Session
+    """
+
     storage.close()
 
 
 @app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
-    """ HBNB is alive! """
+    """HBNB is alive!
+    """
+
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
